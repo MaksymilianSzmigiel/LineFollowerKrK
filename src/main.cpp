277917600dcd167750d802ba10808b0b19f04086
Jakub Żurawski <s193353@student.pg.edu.pl>
2024-11-07 20:25:10 +0100

    silniki działają

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ Motor motor;
 PrivateServer ser;
 
 // Define PWM parameters
-const int pwmFreq = 5000;       // Frequency of 5 kHz
+const int pwmFreq = 1000;       // Frequency of 5 kHz
 const int pwmResolution = 8;    // 8-bit resolution (values range from 0 to 255)
 const int pwmChannelA = 7;      // PWM channel for motor A
 const int pwmChannelB = 6;      // PWM channel for motor B
@@ -20,23 +20,25 @@ void setup() {
   motor.initialize();
   ser.initialize();
   motor.setSensors();
-  digitalWrite(motor.AIN1,HIGH);
-  digitalWrite(motor.BIN1,HIGH);
- digitalWrite(motor.AIN2,LOW);
-  digitalWrite(motor.BIN2,LOW);
+ digitalWrite(motor.AIN1,LOW);
+ digitalWrite(motor.BIN1,LOW);
+  digitalWrite(motor.AIN2,HIGH);
+   digitalWrite(motor.BIN2,HIGH);
 
-   ledcSetup(pwmChannelA,pwmFreq, pwmResolution);
-   ledcSetup(pwmChannelB,pwmFreq, pwmResolution);
-  // ledcSetup(pwmChannelC,pwmFreq,pwmResolution);
+//    ledcSetup(pwmChannelA,pwmFreq, pwmResolution);
+//    ledcSetup(pwmChannelB,pwmFreq, pwmResolution);
+//   // ledcSetup(pwmChannelC,pwmFreq,pwmResolution);
    
-  // Przypisanie kanału PWM do pinu
- // ledcAttachPin(motor.B_PWM,pwmChannelB);
-  ledcAttachPin(motor.A_PWM, pwmChannelA);
-  ledcAttachPin(pin2,pwmChannelC);
+//   // Przypisanie kanału PWM do pinu
+//   ledcAttachPin(motor.B_PWM,pwmChannelB);
+//   ledcAttachPin(motor.A_PWM, pwmChannelA);
+//   ledcAttachPin(pin2,pwmChannelC);
 
-  ledcWrite(pwmChannelA,120);
-  ledcWrite(pwmChannelB,120);
-  //ledcWrite(pwmChannelC,70);
+//   ledcWrite(pwmChannelA,120);
+//   ledcWrite(pwmChannelB,120);
+//   //ledcWrite(pwmChannelC,70);
+ analogWrite(motor.A_PWM,120);
+analogWrite(motor.B_PWM,120);
 
 
 
@@ -46,7 +48,10 @@ void setup() {
 }
 
 void loop() {
+analogWrite(motor.A_PWM,120);
+analogWrite(motor.B_PWM,120);
+  delay(100);
  // ser.clients();
   // Update sensor readings
-  motor.updateSensors();
+ // motor.updateSensors();
 }
